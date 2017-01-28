300e88095040c85a5261b6971f8084ae7d0b46ef
herbzhao <herbzhao@gmail.com>
2017-01-28 12:36:39 +0000

    colour code

---
+++ resistor_colour_code.py
@@ -0,0 +1,6 @@
+resistor_digits = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'purple': 7, 'grey': 8, 'white': 9}
+resistor_multiplier = {'silver': 0.01, 'gold': 0.1, 'black': 10**0, 'brown': 10**1, 'red': 10**2, 'orange': 10**3, 'yellow': 10**4, 'green': 10**5, 'blue': 10**6, 'purple': 10**7}
+resistor_tolerance = {'no band': 20, 'silver': 10, 'gold': 5,'brown': 1, 'red': 2, 'green': 0.5, 'blue': 0.25, 'purple': 0.1}
+resistor_temperature_coefficient = {'brown': 100, 'red': 50, 'orange': 15, 'yellow': 25}
+
+print(resistor_digits['white'])
